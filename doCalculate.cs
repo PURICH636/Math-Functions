efd119fbe11aafd52c2c665125a7e01ef29a536f
3uch <devil.playboys@gmail.com>
2016-02-07 21:29:49 +0700

    Added Factorial function and rename class calculate

--- doCalculate.cs
+++ doCalculate.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Calculator
+    class doCalculate
     {
         static void Main(string[] args)
         {
---
+++ doFactorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class doFactorial
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factorial is : " + factorial(input));
+            Console.ReadKey();
+        }
+
+        public static long factorial(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * factorial(number - 1);
+            }
+        }
+	}
+}
