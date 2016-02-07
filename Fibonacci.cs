c883bb8386252c13dbce94ce82f456ccd87113ed
3uch <devil.playboys@gmail.com>
2016-02-07 21:44:26 +0700

    Added Fibonacci function

---
+++ Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fiboonacci
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factorial is : " + doFibonacci(input));
+            Console.ReadKey();
+        }
+
+        public static long doFibonacci(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+            else
+            {
+                return doFibonacci(number - 1) + doFibonacci(number - 2);
+            }
+        }
+    }
+
+}
